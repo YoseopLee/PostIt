e1a1b920a130dace0885a7e063999a6c173b1d22
YoseopLee <80019249+YoseopLee@users.noreply.github.com>
2023-04-18 08:44:29 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import axios from "axios";
+import AddPost from "./AddPost";
+import Post from "./Post";
+import { useQuery } from "@tanstack/react-query";
+import { PostType } from "./types/Posts";
+
+//Fetch all posts
+const allPosts = async () => {
+  const response = await axios.get("/api/posts/getPosts");
+  return response.data;
+};
+
+export default function Home() {
+  const { data, error, isLoading } = useQuery<PostType[]>({
+    queryFn: allPosts,
+    queryKey: ["posts"],
+  });
+
+  if (error) return error;
+  if (isLoading) return "Loading...";
+  return (
+    <main>
+      <AddPost />
+      {data?.map((post) => (
+        <Post
+          comments={post.comments}
+          key={post.id}
+          name={post.user.name}
+          avatar={post.user.image}
+          postTitle={post.title}
+          id={post.id}
+        />
+      ))}
+    </main>
+  );
+}
